5e6fa84aa584eda66683d9297bd11240f0627b7a
Kidon Son <kidonson@gmail.com>
2017-02-13 19:57:36 -0500

    Added input error handling

--- Sunrise/sunrise.cc
+++ Sunrise/sunrise.cc
@@ -1,10 +1,11 @@
 // Sunrise.cpp : Defines the entry point for the console application.
 //
 
+#include <vector>
+
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/asio.hpp>
 #include <boost/program_options.hpp>
-#include <vector>
 
 #include <LifxControl/light.h>
 #include <LifxControl/lifx_control.h>
@@ -26,29 +27,37 @@ address_v4 kLocalhost(0xC0A80102);
 int main(int ac, char* av[]) {
 	LifxControl lifx_control(kLocalhost, kSubnet);
 
-  po::options_description desc("Allowed options");
-  desc.add_options()
-    ("help", "produces help message")
-    ("enumerate", "detects LIFX devices on the network")
-    ("power", po::value<bool>(), "set power");
+  try {
+    po::options_description desc("Allowed options");
+    desc.add_options()
+      ("help", "produces help message")
+      ("enumerate", "detects LIFX devices on the network")
+      ("power", po::value<bool>(), "set power");
 
-  po::variables_map vm;
-  po::store(po::parse_command_line(ac, av, desc), vm);
-  po::notify(vm);
+    po::variables_map vm;
+    po::store(po::parse_command_line(ac, av, desc), vm);
+    po::notify(vm);
 
-  if (vm.count("help")) {
-    std::cout << desc << std::endl;
-  }
-
-  if (vm.count("enumerate")) {
-    std::vector<Light> lights = lifx_control.Enumerate(seconds(1));
-    for (auto it = lights.begin(); it != lights.end(); ++it) {
-      std::cout << (*it).address() << std::endl;
+    if (vm.count("help")) {
+      std::cout << desc << std::endl;
     }
-  }
 
-  if (vm.count("power")) {
-    std::cout << "power " << vm["power"].as<bool>() << std::endl;
+    if (vm.count("enumerate")) {
+      std::vector<Light> lights = lifx_control.Enumerate(seconds(1));
+      for (auto it = lights.begin(); it != lights.end(); ++it) {
+        std::cout << (*it).address() << std::endl;
+      }
+    }
+
+    if (vm.count("power")) {
+      std::cout << "power " << vm["power"].as<bool>() << std::endl;
+    }
+  } catch (const std::exception& e) {
+    std::cerr << "error: " << e.what() << "\n";
+    return 1;
+  } catch (...) {
+    std::cerr << "Exception of unknown type!\n";
+    return 1;
   }
-	return 0;
+  return 0;
 }
